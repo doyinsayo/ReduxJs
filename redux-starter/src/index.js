2c7fb3fd450ae10c0cd5bfade5aa6b20d6a3c51b
Doyinsayo Otufadebo <64381567+doyinsayo@users.noreply.github.com>
2022-03-30 15:51:51 +0100

    immutability with objects

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+//import { compose, pipe } from 'lodash/fp';
+
+//let input = "   Javascript   ";
+//let output = "<div>" + input.trim() + "</div>";
+
+//const trim = str => str.trim();
+//const wrap = (type,str) => '<${type}>${str}</${type}>';
+//const toLowerCase = str => str.toLowerCase();
+
+//const transform = pipe(trim, toLowerCase, wrap);
+//console.log(transform(input));
+
+const person = { 
+    name: 'John',
+    address: {
+        country: 'USA',
+        city: 'San Francisco'
+    }
+};
+
+const updated = {...person,
+    address: {
+        ...person.address,
+        city: 'Illinois'
+    },
+    name:"Bob"};
+console.log(updated)
--- redux-starter/webpack.config.js
+++ redux-starter/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,9 +8,13 @@ module.exports = {
     filename: "app.js",
     path: path.resolve(__dirname, "dist")
   },
+  module: {
+    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+  },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     port: 9000
   },
+  //plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
   mode: "development"
 };
